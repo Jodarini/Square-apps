c409517ea277522c265cfa4189edfe945251e515
jodarini <rivasjose9428@gmail.com>
2024-08-16 18:05:09 -0500

    Chore: Render multiple squares and some styling

--- square-app-vue/src/App.vue
+++ square-app-vue/src/App.vue
@@ -1,25 +1,37 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import SquareComp from './components/SquareComp.vue'
+
+const squares = [
+  {
+    id: crypto.randomUUID(),
+    color: 'green'
+  },
+  {
+    id: crypto.randomUUID(),
+    color: 'red'
+  },
+  {
+    id: crypto.randomUUID(),
+    color: 'red'
+  }
+]
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <SquareComp v-for="square in squares" :key="square.id"></SquareComp>
   </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
 }
 
 .logo {
---
+++ square-app-vue/src/components/SquareComp.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isRed = ref('red')
+</script>
+
+<template>
+  <div :class="{ red: isRed }"></div>
+</template>
+
+<style scoped>
+div {
+  width: 50px;
+  height: 50px;
+  background: green;
+}
+
+.red {
+  background: red;
+}
+
+.green {
+  background: green;
+}
+</style>
